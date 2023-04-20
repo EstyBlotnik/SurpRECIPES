a7666fd477bd33406a39b3338b6d676d8adcba10
Esty <estyblotnik@gmail.com>
2023-04-20 13:45:50 +0300

    dir

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const path = require('path');
+const filePath = path.join(__dirname, 'public', 'home_page.html');
+
 
 app.get('/', (req, res) => {
     console.log('A new request has arrived to index.js');
-    res.sendFile('home_page.html', { root: '.' });
+    res.sendFile(filePath);
     // res.sendFile(__dirname + '/home_page.html');
     res.send('Hello from the server main page');
 });
