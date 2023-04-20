159227abc5b2f66d69c56762a4cd40759bd6d341
student <adi0548426024@gmail.com>
2023-04-21 00:11:16 +0300

    fix linter

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const __dirname = dirname(__filename);
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname + "/home_page.html")
-    console.log(__dirname)
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/home_page.html`);
+    console.log(__dirname);
 });
 app.listen(port, () => {
     console.log(`Server is up and running at port: ${port}`);
