1278c98d07006bde8f5cafaa70c4457048c5bf1b
student <adi0548426024@gmail.com>
2023-04-27 23:18:05 +0300

    fix lint

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-
 const express = require('express');
+
 const app = express();
-const path = require("path");
+const path = require('path');
 
-
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
- 
     res.render('index');
 });
 app.get('/log_in.ejs', (req, res) => {
- 
     res.render('log_in');
 });
 
