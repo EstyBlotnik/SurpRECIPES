68cac2addd6ffd5bfac3d79395e1f6f0c4baef19
student <adi0548426024@gmail.com>
2023-04-30 23:41:20 +0300

    fix lint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const User = require('../models/users');
+
 const port = process.env.PORT || 3000;
 
-//connect to mongoDB
+// connect to mongoDB
 const dbURI = 'mongodb+srv://pm:1234567890@projectmanagement.srxmxoq.mongodb.net/project_management?retryWrites=true&w=majority';
 mongoose.connect(dbURI).then((result) => app.listen(port, () => {
     console.log(`Server is up and running at port: ${port}`);
-})).catch(err => console.log(err))
+    console.log(result);
+})).catch((err) => console.log(err));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,27 +21,27 @@ app.use(express.static('public'));
 // mongoose & mongo tests
 app.get('/add-user', (req, res) => {
     const user = new User({
-      name: 'new blog',
-      id: 'about my new blog',
-      age: 'more about my new blog'
-    })
-    user.save()
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
+        name: 'new blog',
+        id: 'about my new blog',
+        age: 'more about my new blog',
     });
+    user.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 });
 app.get('/all-blogs', (req, res) => {
     User.find()
-      .then(result => {
-        res.send(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -49,5 +52,3 @@ app.get('/log_in.ejs', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
-
