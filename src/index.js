3899027848651dd881b34a3c4d310fbc3740c641
student <adi0548426024@gmail.com>
2023-04-30 21:04:03 +0300

    add static file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
