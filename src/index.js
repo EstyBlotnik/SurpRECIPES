3fd82d475fbf8e60f5bf29b74324d170f5217a69
student <adi0548426024@gmail.com>
2023-05-16 22:35:34 +0300

    add modul sign up and sign in

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
 const connectToDatabase = require('./db/db');
 const routes = require('./Routes/routes');
+const authRouter = require('./Routes/authRoutes');
 
 const port = process.env.PORT || 3000;
 
@@ -17,10 +17,17 @@ connectToDatabase()
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error);
     });
-
+app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 // Register routes
 app.use('/', routes);
+app.use('/auth', authRouter);
+app.get('/recipe', (req, res) => {
+    // Handle GET request for '/users' route
+    // Retrieve data, perform operations, etc.
+    // Send a response back to the client
+    res.render('recipeUpload');
+  });
