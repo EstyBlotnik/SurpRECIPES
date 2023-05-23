b284927a476d67bb80cba092bd692a15838549da
Demi Netzer <demi.netzer8@gmail.com>
2023-05-23 09:43:20 +0300

    fix recipe include

--- src/Routes/viewRecipeRout.js
+++ src/Routes/viewRecipeRout.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Recipe = require('../models/recipeSchema');
+const Recipe = require('../models/recipe');
 
 router.get('/', (req, res) => {
     Recipe.find()
