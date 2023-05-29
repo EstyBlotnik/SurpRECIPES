518e5dc97b3a83c8567d5500e613d38b7926bec9
student <adi0548426024@gmail.com>
2023-05-29 11:18:38 +0300

    fix login

--- src/Controllers/controllers.js
+++ src/Controllers/controllers.js
@@ -3,10 +3,6 @@ exports.renderIndex = (req, res) => {
   res.render('index');
 };
 
-exports.renderLogIn = (req, res) => {
-  const message = req.flash('message')[0];
-  res.render('login', { error: '' });
-};
 
 exports.renderAbout = (req, res) => {
 
