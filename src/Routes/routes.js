aafdc127bba46ef88123101216e106864ce97d65
shirelabergil <ghanaab@ac.sce.ac.il>
2023-06-12 13:19:28 +0300

    followersfollowing list again ! dont distroid it !

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -98,36 +98,7 @@ router.post('/viewOtherProfile', async (req, res) => {
   console.error(error);
   res.status(500).send('Internal Server Error');
 }
-
-  res.render('user_profile', { mongo_user: user, current_user: req.user });
 });
-
-router.get('/user_profile', (req, res) => {
-  if (req.user) {
-    res.render('user_profile', { mongo_user: req.user, current_user: req.user });
-  }
-});
-
-
-// router.get('/user_profile', async (req, res) => {
-//   if (req.user) {
-
-//     try {
-//       // Retrieve the saved recipe object IDs
-//       const followersIds = req.user.followers;
-//       const followingsIds = req.user.followedUsers;
-
-//       // Fetch the saved recipes from the database
-//       const followers = await User.find({ _id: { $in: followersIds } });
-//       const followings = await User.find({ _id: { $in: followingsIds } });
-//       res.render('user_profile', { followers: followers, followings: followings, mongo_user: req.user });
-//     } catch (error) {
-//       // Handle the error appropriately
-//       console.error(error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   }
-// });
 
 router.get('/uploadrecipe', (req, res) => {
   if (req.user) {
